0aad9d93c6750181b8ac61455b21b40725712992
Leanne <sooyeunleanne@gmail.com>
2025-05-06 20:48:07 -0700

    calculate hash function

--- Block.py
+++ Block.py
@@ -1,14 +1,14 @@
-import hashlib
-import time
+import hashlib #provides algo for hashing, such as sha256
+import time 
 import json
 
 class Block:
-    def __init__(self, index, data, previous_hash=''):
+    def __init__(self, index, data, previous_hash=''): #constructor
         self.index = index
         self.timestamp = time.time()
         self.data = data
-        self.previous_hash = previous_hash
-        self.hash = self.calculate_hash()
+        self.previous_hash = previous_hash #이전 블록 해시
+        self.hash = self.calculate_hash() #현재 블록의 고유 해시 값
     
     def calculate_hash(self):
         block_string = json.dumps({
@@ -16,4 +16,6 @@ class Block:
             'timestamp': self.timestamp,
             'data': self.data,
             'previous_hash': self.previous_hash
-        })
+        }, sort_keys = True).encode()
+
+        return hashlib.sha256(block_string).hexdigest()
